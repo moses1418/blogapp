a8a493ffbb3cb34c2dbf7af4ae2b554e10fefd82
moses1418 <mosesrodriguez1414@yahoo.com>
2021-08-11 12:18:04 -0500

    viewController

---
+++ src/main/java/com/codeup/blogapp/HelloController.java
@@ -0,0 +1,55 @@
+package com.codeup.blogapp;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello, from spring!";
+    }
+
+
+    @GetMapping("/person")
+    @ResponseBody
+    public Person person(){
+        return new Person("Moses", "bum");
+    }
+
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name){
+        return "hello, " + name + "!";
+    }
+
+
+    @GetMapping("/home")
+    public String welcome(){
+        return "home";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
