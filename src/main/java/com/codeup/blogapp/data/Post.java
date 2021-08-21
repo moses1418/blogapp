0887ece801a53c9b2f7981aac867ef5094378aef
moses1418 <mosesrodriguez1414@yahoo.com>
2021-08-20 23:23:59 -0500

    blogapp updates

--- src/main/java/com/codeup/blogapp/data/Post.java
+++ src/main/java/com/codeup/blogapp/data/Post.java
@@ -1,14 +1,25 @@
 package com.codeup.blogapp.data;
 
+import javax.persistence.*;
 import java.util.Collection;
 
+@Entity
+@Table(name = "posts")
 public class Post {
-    private Long id;
-    private String title;
-    private String content;
-    private User user;
-    private Collection<Category> categories;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private User user;
+
+    private Collection<Category> categories;
 
 
     public Post(Long id, String title, String content, User user, Collection<Category> categories) {
